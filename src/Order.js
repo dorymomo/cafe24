44abf96da3e08c0c6cbc27b0668807be1aa5e977
chang pyo <ckdvy0629@gmail.com>
2023-01-30 17:37:39 +0900

    second commit

--- src/Order.js
+++ src/Order.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
 
+
 const order = () => {
     
     return (
-        <p>
-            <h4>주문 정보 </h4>
-            
-            <label>수량을 선택
-                <input type="number" name="number_select" min="1" max="10" ></input> 
-                
-            </label>
-            
-            <input type="submit" value="주문확인"/>
-            
-        </p>
-        
-        
+      <p>
+        <h4>주문 정보 </h4>
+
+        <label>
+          수량을 선택
+          <input type="number" name="number_select" min="1" max="10"></input>
+        </label>
+
+        <button onClick={()=>{alert('구매완료')}}>주문확인</button>
+
+      </p>
     );
 };
-
 
 export default order;
